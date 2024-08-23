6fb85059588592e7b9418e55ade6e9f5efa9cf44
Alex Turbov <i.zaufi@gmail.com>
2024-08-23 06:06:04 +0400

    feat: add `check-pre-commit` target for `git` plugin

--- extras.d/git-plugin.mk
+++ extras.d/git-plugin.mk
@@ -1,7 +1,13 @@
 git-st:
 	$(MAKE) for-each-working-tree exec='git status'
 
+check-pre-commit:
+	$(MAKE) for-each-working-tree exec='pre-commit run -a $(hook)'
+
 _help_Git:
 	$(call fn.hlp.tgt, make git-st [match=<pattern>], check Git status of all working trees)
+	$(call fn.hlp.tgt, make check-pre-commit, run $(c.white)pre-commit$(c.normal) hooks check for all working trees)
+	$(call fn.hlp.tgt, $(help.use2.indent)[match=<pattern>], that match an optional pattern)
+	$(call fn.hlp.tgt, $(help.use2.indent)[hook=<name>])
 
 $(call fn.set.plugin.help.target,_help_Git)
